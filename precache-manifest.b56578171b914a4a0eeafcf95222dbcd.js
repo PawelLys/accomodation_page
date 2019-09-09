1ba997ee77053f0419acb387a96b022f65358c66
PawelLys <53062631+PawelLys@users.noreply.github.com>
2019-09-10 00:33:51 +0200

    Updates

---
+++ precache-manifest.b56578171b914a4a0eeafcf95222dbcd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee1c3c537a58241189b10dd0b150fbb",
+    "url": "/accomodation_page/index.html"
+  },
+  {
+    "revision": "c37dafdea0147855e10b",
+    "url": "/accomodation_page/static/js/2.b89893e2.chunk.js"
+  },
+  {
+    "revision": "78a8df3e505bc5f5d206",
+    "url": "/accomodation_page/static/js/main.a903fa99.chunk.js"
+  },
+  {
+    "revision": "7c090590a014aba0f110",
+    "url": "/accomodation_page/static/js/runtime~main.2dc318ff.js"
+  }
+]);
